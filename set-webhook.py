6773037a8bd2f562f90ea656b55ef83698806500
ceresgalax <ceresgalax@gmail.com>
2023-12-30 12:26:22 -0800

    Fix set-webhook.py to allow running from any working directory, lint.

--- set-webhook.py
+++ set-webhook.py
@@ -8,9 +8,13 @@ import sys
 import urllib.request
 import subprocess
 
-NGROK_TARGET_PORT = re.search('host.docker.internal:([0-9]+)', pathlib.Path('docker-compose.yaml').read_text()).group(1)
+
+docker_compose_path = os.path.normpath(os.path.join(__file__, '..', 'docker-compose.yaml'))
+
+NGROK_TARGET_PORT = re.search('host.docker.internal:([0-9]+)', pathlib.Path(docker_compose_path).read_text()).group(1)
 
 os.chdir(os.path.normpath(os.path.join(__file__, '..')))
+
 
 def get_data_from_terraform():
     p = subprocess.run(
@@ -23,7 +27,7 @@ def get_data_from_terraform():
     bot_token = outputs["telegram_bot_token"]["value"]
     webhook_url = outputs["webhook_url"]["value"]
     webhook_secret = outputs["webhook_secret"]["value"]
-    return (bot_token, webhook_url, webhook_secret)
+    return bot_token, webhook_url, webhook_secret
 
 
 def get_data_from_local():
@@ -37,36 +41,42 @@ def get_data_from_local():
     ][0]
     webhook_url = base_url + "/webhook"
     webhook_secret = "NONE"
-    return (bot_token, webhook_url, webhook_secret)
+    return bot_token, webhook_url, webhook_secret
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("context", choices=["terraform", "local"])
-args = parser.parse_args()
 
-if args.context == "terraform":
-    fn = get_data_from_terraform
-elif args.context == "local":
-    fn = get_data_from_local
-else:
-    sys.exit(2)
-bot_token, webhook_url, webhook_secret = fn()
-print(f"Webhook URL: {webhook_url}")
 
-request = urllib.request.Request(
-    url=f'https://api.telegram.org/bot{bot_token}/setWebhook',
-    data=json.dumps({
-        'url': webhook_url,
-        'secret_token': webhook_secret
-    }).encode('utf-8'),
-    headers={
-        'Content-Type': 'application/json',
-        'User-Agent': 'barbot by @ceresgalax'
-    },
-    method='POST'
-)
+def main():
+    args = parser.parse_args()
+    if args.context == "terraform":
+        fn = get_data_from_terraform
+    elif args.context == "local":
+        fn = get_data_from_local
+    else:
+        sys.exit(2)
+    bot_token, webhook_url, webhook_secret = fn()
+    print(f"Webhook URL: {webhook_url}")
 
-with urllib.request.urlopen(request) as response:
-    if response.status < 200 or response.status >= 300:
-        print('Failed to set webhook')
-        exit(1)
+    request = urllib.request.Request(
+        url=f'https://api.telegram.org/bot{bot_token}/setWebhook',
+        data=json.dumps({
+            'url': webhook_url,
+            'secret_token': webhook_secret
+        }).encode('utf-8'),
+        headers={
+            'Content-Type': 'application/json',
+            'User-Agent': 'barbot by @ceresgalax'
+        },
+        method='POST'
+    )
+
+    with urllib.request.urlopen(request) as response:
+        if response.status < 200 or response.status >= 300:
+            print('Failed to set webhook')
+            exit(1)
+
+
+if __name__ == "__main__":
+    main()

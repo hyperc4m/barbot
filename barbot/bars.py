a12f90a78b1340d1be07822b9ad486747a45c65b
ceresgalax <ceresgalax@gmail.com>
2023-12-30 21:56:55 -0800

    Lint and fix rare case where csv data may have a header
    * Also added a rudimentary unit test for parsing bar csv data.

--- barbot/bars.py
+++ barbot/bars.py
@@ -6,7 +6,6 @@ import unittest
 import urllib.request
 from typing import NamedTuple, List, Optional, Tuple, Sequence, Set
 
-from . import app
 
 CACHE_LIFETIME = datetime.timedelta(seconds=60)
 
@@ -36,24 +35,32 @@ def _normalize_spreadsheet_url(url: str) -> str:
 def _fetch_bars(url: str) -> List[Bar]:
     with urllib.request.urlopen(url) as response:
         data = response.read().decode("utf-8")
-    reader = csv.DictReader(data.splitlines())
+        return _parse_bars(data)
+
+
+def _parse_bars(csv_str: str) -> List[Bar]:
+    reader = csv.DictReader(csv_str.splitlines())
     bars = []
     for row in reader:
-        try:
-            row["latitude"] = float(row["latitude"])
-            row["longitude"] = float(row["longitude"])
-            bars.append(
-                Bar(
-                    name=row["name"],
-                    address=row["address"],
-                    latitude=float(row["latitude"]),
-                    longitude=float(row["longitude"]),
-                    plus_code=row["plus_code"],
-                    aliases=set(a for a in row["aliases"].split("|") if a),
+        if isinstance(row, str):
+            # Skip possible header row in csv data
+            pass
+        elif isinstance(row, dict):
+            try:
+                row["latitude"] = float(row["latitude"])
+                row["longitude"] = float(row["longitude"])
+                bars.append(
+                    Bar(
+                        name=row["name"],
+                        address=row["address"],
+                        latitude=float(row["latitude"]),
+                        longitude=float(row["longitude"]),
+                        plus_code=row["plus_code"],
+                        aliases=set(a for a in row["aliases"].split("|") if a),
+                    )
                 )
-            )
-        except Exception as err:
-            print(f"Bad bar specification (`{err}`): `{row}`")
+            except Exception as err:
+                print(f"Bad bar specification (`{err}`): `{row}`")
     return bars
 
 
@@ -98,7 +105,7 @@ class Bars:
                 known.append(bar)
             else:
                 unknown.append(search)
-        return (unknown, known)
+        return unknown, known
 
 
 class TestBars(unittest.TestCase):
@@ -136,3 +143,25 @@ class TestBars(unittest.TestCase):
             canonical,
             _normalize_spreadsheet_url("docs.google.com/spreadsheets/d/1QBk_HhV6"),
         )
+
+    def test_parse_bars(self):
+        data = '''name,address,latitude,longitude,plus_code,aliases
+Blackbird,"2124 Market St, San Francisco, CA 94114",37.76725382,-122.429588,"QH8C+W5 Duboce Triangle, San Francisco, CA",
+Churchill Cocktail Bar,"198 Church St, San Francisco, CA 94114",37.7678905,-122.4291782,"QH9C+58 Duboce Triangle, San Francisco, CA",churchill
+Finnegans Wake,"937 Cole St, San Francisco, CA 94117",37.76529613,-122.4501335,"QG8X+4W Cole Valley, San Francisco, CA",finnegans
+Hobson's Choice,"1601 Haight St, San Francisco, CA 94117",37.76968435,-122.4487544,"QH92+VG Haight-Ashbury, San Francisco, CA",hobsons
+Last Rites,"718 14th St, San Francisco, CA 94114",37.7678973,-122.4294669,"QH9C+56 Duboce Triangle, San Francisco, CA",last rights
+Local Edition,"691 Market St, San Francisco, CA 94105",37.78764413,-122.4031763,"QHQW+2P Yerba Buena, San Francisco, CA",
+Noc Noc,"557 Haight St, San Francisco, CA 94117",37.7718871,-122.4314015,"QHC9+PC Lower Haight, San Francisco, CA",nock nock|knock knock
+Pagan Idol,"375 Bush St, San Francisco, CA 94104",37.79072444,-122.4036048,"QHRW+7J Union Square, San Francisco, CA",pagan idle|pagin idol|pagin idle
+Pilsner Inn,"225 Church St, San Francisco, CA 94114",37.76711585,-122.4287876,"QH8C+RF Mission Dolores, San Francisco, CA",pilsner
+SF Eagle,"398 12th St, San Francisco, CA 94103",37.76999463,-122.4134,"QH9P+XJ SoMa, San Francisco, CA",eagle|the eagle|eagle sf|sf eagle|the sf eagle|the eagle sf
+Smuggler's Cove,"650 Gough St, San Francisco, CA 94102",37.7794161,-122.4233717,"QHHG+QM Fillmore District, San Francisco, CA",smugglers|smuggler
+Standard Deviant Brewing,"280 14th St, San Francisco, CA 94103",37.76840917,-122.4194822,"QH9J+96 Mission District, San Francisco, CA",standard deviant
+The Irish Bank,"10 Mark Ln, San Francisco, CA 94108",37.79039514,-122.4045096,"QHRW+56 Union Square, San Francisco, CA",irish bank
+The Zombie Village,"441 Jones St, San Francisco, CA 94102",37.78551504,-122.4132064,"QHPP+6P Tenderloin, San Francisco, CA",zombie village
+Trick Dog,"3010 20th St, San Francisco, CA 94110",37.75921458,-122.4111932,"QH5Q+MG Mission District, San Francisco, CA",
+Upcider,"1160 Polk St 2nd floor, San Francisco, CA 94109",37.78753092,-122.4198176,"QHQJ+23 Lower Nob Hill, San Francisco, CA",
+Zeitgeist,"199 Valencia St, San Francisco, CA 94103",37.77002787,-122.4221187,"QHCH+25 SoMa, San Francisco, CA",'''
+        bars = _parse_bars(data)
+        self.assertEqual(17, len(bars))

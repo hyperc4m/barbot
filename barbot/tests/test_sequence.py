b8efb6f358833638c69a6d9f36a23f09b32ff988
ceresgalax <ceresgalax@gmail.com>
2025-02-16 12:48:50 -0800

    Add a test for choosing a winning bar that has markdown special characters in it's name.

--- barbot/tests/test_sequence.py
+++ barbot/tests/test_sequence.py
@@ -1,4 +1,5 @@
 import unittest
+from typing import Sequence
 from unittest.mock import AsyncMock, ANY, MagicMock
 
 import telegram
@@ -17,23 +18,22 @@ class MockServices(object):
         bot.return_value.stop_poll = AsyncMock()
         bot.return_value.send_message = AsyncMock()
         bot.return_value.pin_chat_message = AsyncMock()
-        bot.return_value.stop_poll.return_value = telegram.Poll(
+        self.bot = bot
+
+        scheduler = MagicMock()
+        self.scheduler = scheduler
+
+    def configure_stop_poll(self, options: Sequence[telegram.PollOption]):
+        self.bot.return_value.stop_poll.return_value = telegram.Poll(
             id='1',
             question='',
-            options=[
-                telegram.PollOption('Foo', 5),
-                telegram.PollOption('Bar', 6),  # pun intended
-            ],
+            options=options,
             total_voter_count=0,
             is_closed=True,
             is_anonymous=False,
             type='',
             allows_multiple_answers=True
         )
-        self.bot = bot
-
-        scheduler = MagicMock()
-        self.scheduler = scheduler
 
     def make_services(self) -> SequenceServices:
         return SequenceServices(self.db(), self.bot(), self.scheduler())
@@ -42,10 +42,27 @@ class MockServices(object):
 class TestChooseWinner(unittest.IsolatedAsyncioTestCase):
     async def test_typical_bar(self):
         mock_services = MockServices()
+        mock_services.configure_stop_poll([
+            telegram.PollOption('Foo', 5),
+            telegram.PollOption('Bar', 6),  # pun intended
+        ])
 
         result = await sequence.handle_choose_winner({}, mock_services.make_services())
 
         expected_message = 'Calling it for *Bar*\\!'
+        mock_services.bot.return_value.send_message.assert_called_with(
+            chat_id=ANY, text=expected_message, parse_mode='MarkdownV2',
+            disable_web_page_preview=ANY, reply_to_message_id=ANY)
+
+    async def test_bar_with_markdown_characters(self):
+        mock_services = MockServices()
+        mock_services.configure_stop_poll([
+            telegram.PollOption('Foo', 5),
+            telegram.PollOption('Dingles!?.', 6),
+        ])
+
+        result = await sequence.handle_choose_winner({}, mock_services.make_services())
+        expected_message = 'Calling it for *Dingles\\!?\\.*\\!'
         mock_services.bot.return_value.send_message.assert_called_with(
             chat_id=ANY, text=expected_message, parse_mode='MarkdownV2',
             disable_web_page_preview=ANY, reply_to_message_id=ANY)

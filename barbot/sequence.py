296ac0672995893129f9f504a93def33e3e2d9bb
ceresgalax <ceresgalax@gmail.com>
2025-02-16 12:30:59 -0800

    Fix usages of schedule_util to include the scheduler.

--- barbot/sequence.py
+++ barbot/sequence.py
@@ -36,7 +36,7 @@ def handle_function_call(event: Dict[str, Any], context: Dict[str, Any]) -> Dict
 async def handle_ask_for_suggestions(event: Dict[str, Any], services: SequenceServices) -> Dict[str, Any]:
     text = f'It\'s time for bar night suggestions! Message @{app.BOT_USERNAME} or end a message with ' \
            f'{app.BARNIGHT_HASHTAG} to input a suggestion!'
-    poll_time = schedule_util.get_schedule_time(app.CREATE_POLL_SCHEDULE_NAME)
+    poll_time = schedule_util.get_schedule_time(services.scheduler, app.CREATE_POLL_SCHEDULE_NAME)
     if poll_time:
         text += f' Poll will be created on {poll_time}.'
 
@@ -112,7 +112,7 @@ async def handle_poll_reminder(event: Dict[str, Any], services: SequenceServices
         return {}
 
     text = "REMINDER: Don't forget to vote!"
-    close_time = schedule_util.get_schedule_time(app.CLOSE_POLL_SCHEDULE_NAME)
+    close_time = schedule_util.get_schedule_time(services.scheduler, app.CLOSE_POLL_SCHEDULE_NAME)
     if close_time:
         text += f' The poll will close on {close_time}.'
 
